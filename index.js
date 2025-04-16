a3c5e804449c3316c347838ed058bc64b1a64500
Stepan Kormilin <ubscoder@gmail.com>
2025-04-16 12:10:06 +0300

    HW-6

--- index.js
+++ index.js
@@ -1,45 +1,79 @@
-/*
-1."кот" > "код"
-true: строка с символом, имеющим больший код в Unicode, считается "большей"
-2. "2" + 2 * "2"
-"24": 2 * "2" -> 4; "2" + 4 -> "24" приведение типов
-3. undefined == null
-true: при нестрогом сравнении JavaScript выполняет преобразование типов, и null и undefined считаются равными
-4. undefined != null
-false: предыдущий пример + !
-5. null == 0
-false: null рассматривается как "пустое" значение, которое не является числом
-6. 2 > "3"
-false: JavaScript преобразует строку в число
-7. null - false + true
-1: 0 - 0 + 1
-8. 1 / "l"
-NaN: 1/NaN
-9. "2" * "3"
-6: 2 * 3
-10. 4 + 5 + "O"
-90: 4 + 5 -> 9 + "0" преобразует к строке
+// 1.
+// const strings = ["hello", "world", "JavaScript"];
+// const lengths = strings.map(str => str.length);
 
-11. "l" + 4 + 5
-"l45": "l" + 4 -> "l4" + 5 преобразует к строке
-12. "4" - 2
-2: преобразует к числу
-13. "4" - "4x"
-NaN: "4x" при преобразовании в число NaN
-14. “23” == 23
-true: преобразует к числу
-15. null == false
-false: при нестрогом сравнении null не равен ничему, кроме самого себя и undefined
-16. "-4 "/ 0 + 1
--Infinity: деление числа на 0
-17. null + 1
-1: null преобразуется в 0
-18. undefined + null
-0: 0 + 0
-19. 1 === “1”
-false: строгое равенство, разные типы
-20. “2” > 10
-false: см. пункт 6
-21. NaN == undefined
-false: NaN не равен ничему, включая сам себя
-*/
+// 2.
+// const numbers = [2, 3, 5, 7, 11, 13, 17, 19];
+//
+// function currentSums(numbers) {
+//   return numbers.reduce((acc, curr, index) => {
+//     acc[index] = (acc[index - 1] || 0) + curr;
+//     return acc;
+//   }, []);
+// }
+//
+// const sums = currentSums(numbers);
+
+// 3.
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 7];
+// const pairs = [];
+//
+// for (let i = 0; i <= 7; i++) {
+//   if (numbers.includes(7 - i)) {
+//     pairs.push(`${i}:${7 - i}`);
+//   }
+// }
+
+// 4.
+// const str = "hello world";
+// const firstLetters = str.split(" ").map(word => word[0]);
+
+// 5.
+// const str = "hello";
+// const substrings = [];
+//
+// for (let i = 0; i < str.length; i++) {
+//   const start = Math.max(0, i - 1);
+//   const end = Math.min(str.length, i + 2);
+//   substrings.push(str.substring(start, end));
+// }
+
+// 6.
+// const digits = [3, 5, 1, 4, 2];
+// const sortedDigits = digits.sort((a, b) => b - a);
+
+// 7.
+// const arr1 = [3, 1, 4];
+// const arr2 = [2, 5];
+// const arr3 = [9, 0];
+//
+// const combinedArray = arr1.concat(arr2, arr3).sort((a, b) => b - a);
+
+// 8.
+// const matrix = [[1, 2, 3], [4, 5], [6]];
+// const sum = matrix.flat().reduce((acc, curr) => acc + curr, 0);
+
+// 9.
+// const numbers = [1, 2, 3, 4, 5];
+// const reversed = [];
+// for (let i = numbers.length - 1; i >= 0; i--) {
+//   reversed.push(numbers[i]);
+// }
+
+// 10.
+// const numbers = [1, 2, 3, 4, 5];
+// let sum = 0;
+// let count = 0;
+//
+// for (let num of numbers) {
+//   sum += num;
+//   count++;
+//   if (sum > 10) break;
+// }
+
+// 11.
+// function arrayFill(value, count) {
+//   return Array(count).fill(value);
+// }
+//
+// const filledArray = arrayFill('x', 5);
